504ca44fd042cf541b1b326d5c43d53102a37347
Siam-AR <siam.ar.nexus@gmail.com>
2026-02-24 19:38:22 +0600

    feat(available-jobs): implement job action functionality

--- scripts/script.js
+++ scripts/script.js
@@ -57,3 +57,80 @@ function toggleStyle(id) {
         filterSection.classList.add('hidden');
     }
 }
+
+
+mainContainer.addEventListener('click', function (event) {
+
+    const card = event.target.closest('.relative');
+    if (!card) return;
+
+    const companyName = card.querySelector('.company-name').innerText;
+    const jobTitle = card.querySelector('.job-title').innerText;
+    const jobMeta = card.querySelector('.job-meta').innerText;
+    const description = card.querySelector('.description').innerText;
+    const statusElement = card.querySelector('.job-status');
+
+    if (event.target.innerText === "INTERVIEW") {
+
+        statusElement.innerText = "INTERVIEW";
+        statusElement.classList.remove('bg-primary-content');
+        statusElement.classList.add('bg-green-100');
+
+        const exist = interviewList.find(item => item.companyName === companyName);
+
+        if (!exist) {
+            interviewList.push({
+                companyName,
+                jobTitle,
+                jobMeta,
+                description,
+                status: "INTERVIEW"
+            });
+        }
+
+        rejectedList = rejectedList.filter(item => item.companyName !== companyName);
+
+        if (currentStatus === 'Rejected-filter-btn') renderRejected();
+
+        calculateCount();
+    }
+
+    if (event.target.innerText === "REJECTED") {
+
+        statusElement.innerText = "REJECTED";
+        statusElement.classList.remove('bg-primary-content');
+        statusElement.classList.add('bg-red-100');
+
+        const exist = rejectedList.find(item => item.companyName === companyName);
+
+        if (!exist) {
+            rejectedList.push({
+                companyName,
+                jobTitle,
+                jobMeta,
+                description,
+                status: "REJECTED"
+            });
+        }
+
+        interviewList = interviewList.filter(item => item.companyName !== companyName);
+
+        if (currentStatus === 'interview-filter-btn') renderInterview();
+
+        calculateCount();
+    }
+
+    if (event.target.closest('i')) {
+
+        interviewList = interviewList.filter(item => item.companyName !== companyName);
+        rejectedList = rejectedList.filter(item => item.companyName !== companyName);
+
+        card.remove();
+
+        calculateCount();
+
+        if (currentStatus === 'interview-filter-btn') renderInterview();
+        if (currentStatus === 'Rejected-filter-btn') renderRejected();
+    }
+
+});
