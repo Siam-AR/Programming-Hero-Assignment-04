6bb3c6e30d29fb369aed19af7b50535d818a2ccd
Siam-AR <siam.ar.nexus@gmail.com>
2026-02-24 19:30:46 +0600

    feat(available-jobs): implement filter toggle

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,42 @@ const filterSection = document.getElementById('filtered-section');
 
 
 function calculateCount() {
-    total.innerText = allCardSection.children.length;
+
+    const totalCards = allCardSection.children.length;
+
+    total.innerText = totalCards;
     interviewCount.innerText = interviewList.length;
     rejectedCount.innerText = rejectedList.length;
-    totalJobsText.innerText = allCardSection.children.length;
+    totalJobsText.innerText = totalCards;
+
 }
 
 calculateCount();
+
+
+function toggleStyle(id) {
+
+    allFilterBtn.classList.remove('btn-info', 'text-white');
+    interviewFilterBtn.classList.remove('btn-info', 'text-white');
+    rejectedFilterBtn.classList.remove('btn-info', 'text-white');
+
+    const selected = document.getElementById(id);
+    selected.classList.add('btn-info', 'text-white');
+
+    currentStatus = id;
+
+    if (id === 'interview-filter-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderInterview();
+    }
+    else if (id === 'Rejected-filter-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderRejected();
+    }
+    else {
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    }
+}
